8f18d0c46753a48afd836f7fd528817e237e9b58
david lopez saludes <davidlopez1972@hotmail.es>
2017-10-05 20:27:37 +0200

    Avoidance scene created

---
+++ src/SceneAvoidance.cpp
@@ -0,0 +1,70 @@
+#include "SceneAvoidance.h"
+
+using namespace std;
+
+SceneAvoidance::SceneAvoidance()
+{
+	Agent *agent = new Agent;
+	agent->setPosition(Vector2D(640, 360));
+	agent->setTarget(Vector2D(640, 360));
+	agent->loadSpriteTexture("../res/soldier.png", 4);
+	agents.push_back(agent);
+	target = Vector2D(640, 360);
+}
+
+SceneAvoidance::~SceneAvoidance()
+{
+	for (int i = 0; i < (int)agents.size(); i++)
+	{
+		delete agents[i];
+	}
+}
+
+Vector2D SceneAvoidance::Avoid(Agent *agent, Vector2D target, float dtime) {
+	Vector2D desiredVel;
+	Vector2D steeringForce;
+	desiredVel = target - agent->getPosition();
+
+}
+
+Vector2D SceneAvoidance::Avoid(Agent *agnet, Agent *target, float dtime) {
+
+}
+
+void SceneAvoidance::update(float dtime, SDL_Event *event)
+{
+	/* Keyboard & Mouse events */
+	switch (event->type) {
+	case SDL_MOUSEMOTION:
+	case SDL_MOUSEBUTTONDOWN:
+		if (event->button.button == SDL_BUTTON_LEFT)
+		{
+			target = Vector2D((float)(event->button.x), (float)(event->button.y));
+			agents[0]->setTarget(target);
+		}
+		break;
+	default:
+		break;
+	}
+	//Equals Avoidance function
+	Vector2D steering_force;
+	agents[0]->update(steering_force, dtime, event);
+}
+
+void SceneAvoidance::draw()
+{
+	draw_circle(TheApp::Instance()->getRenderer(), (int)target.x, (int)target.y, 15, 255, 0, 0, 255);
+	agents[0]->draw();
+}
+
+const char* SceneAvoidance::getTitle()
+{
+	return "SDL Steering Behaviors :: Avoidance Demo";
+}
+
+void SceneAvoidance::changeDebugMode() {
+	debugMode != debugMode;
+	for (int i = 0; i < agents.size(); i++) {
+		agents[i]->changeDrawMode();
+	}
+}
--- src/SceneAvoidance.h
+++ src/SceneAvoidance.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Scene.h"
+#include "Agent.h"
+
+class SceneAvoidance :
+	public Scene
+{
+public:
+	SceneAvoidance();
+	~SceneAvoidance();
+	Vector2D Avoid(Agent *agent, Vector2D target, float dtime);
+	Vector2D Avoid(Agent *agent, Agent *target, float dtime);
+	void update(float dtime, SDL_Event *event);
+	void draw();
+	const char*getTitle();
+	void changeDebugMode();
+private:
+	std::vector<Agent*> agents;
+	Vector2D target;
+};
